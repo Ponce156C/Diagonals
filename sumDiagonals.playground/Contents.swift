92a66516bc14e2bdbfdcf156e0e8ae66c5f6fe59
Carlos Ponce <carlosponce156@gmail.com>
2020-08-22 13:40:42 -0500

    calcule and value diagonals

--- sumDiagonals.playground/Contents.swift
+++ sumDiagonals.playground/Contents.swift
@@ -1,3 +1,36 @@
 import UIKit
 
-var str = "Hello, playground"
+class Diagonal {
+    var matrix:[[Int]] = [
+        [1,2,3,4,5],
+        [2,3,4,6,6],
+        [3,4,5,6,7],
+        [4,5,6,10,8],
+        [5,6,7,8,9]
+    ]
+    var mainDiagonalSum = 0
+    var invertedDiagonalSum = 0
+    var isSquare = true
+    
+    func calculediagonal() {
+        for i in 0..<matrix.count {
+            if matrix[i].count == matrix.count {
+                mainDiagonalSum += matrix[i][i]
+                invertedDiagonalSum += matrix[i][matrix.count - 1 - i]
+            }else {
+                isSquare = false
+                break
+            }
+        }
+        if isSquare {
+            print(mainDiagonalSum>invertedDiagonalSum ? "El contador de izquierda-derecha:\(mainDiagonalSum) es mayor que el contador de derecha-izquierda:\(invertedDiagonalSum)": mainDiagonalSum==invertedDiagonalSum ? "los contadores son iguales" : "El contador de derecha-izquierda:\(invertedDiagonalSum) es mayor que el contador izquierda-derecha:\(mainDiagonalSum)")
+        }else {
+            print("La matriz no es cuadrada")
+        }
+    }
+}
+
+let diagonal = Diagonal()
+
+diagonal.calculediagonal()
+
